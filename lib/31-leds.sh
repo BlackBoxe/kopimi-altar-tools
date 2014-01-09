9cba23a2073fe1f9882211312032ee5bfeae0dc3
Nico <nicolas.thill@gmail.com>
2014-01-09 20:48:12 +0100

    register leds hooks if serial port is available

--- lib/31-leds.sh
+++ lib/31-leds.sh
@@ -2,10 +2,13 @@
 
 k_leds_init() {
 	[ -c "$K_LEDS_SERIAL_PORT" ] || \
-		return
+		return 1
 	k_log 3 "initializing leds serial port"
-	stty -F $K_LEDS_SERIAL_PORT $K_LEDS_SERIAL_BAUD_RATE cs8 -cstopb || \
+	stty -F $K_LEDS_SERIAL_PORT $K_LEDS_SERIAL_BAUD_RATE cs8 -cstopb || {\
 		k_log 1 "ERROR: error initializing leds serial port"
+		return 1
+	}
+	return 0
 }
 
 k_leds_update() {
@@ -35,13 +38,13 @@ k_leds_pulse() {
 }
 
 k_leds_startup() {
-	k_leds_init
+	k_leds_init || \
+		return
 	k_leds_pulse
+	k_hook_register_handler on_media_plugged k_leds_off
+	k_hook_register_handler on_media_removed k_leds_pulse
+	k_hook_register_handler on_copy_started k_leds_blink
+	k_hook_register_handler on_copy_ended k_leds_on
 }
-
-k_hook_register_handler on_media_plugged k_leds_off
-k_hook_register_handler on_media_removed k_leds_pulse
-k_hook_register_handler on_copy_started k_leds_blink
-k_hook_register_handler on_copy_ended k_leds_on
 
 k_hook_register_handler on_app_started k_leds_startup
